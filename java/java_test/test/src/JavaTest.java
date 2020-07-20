9cbbe3165d0d0738ed28957f7ad23aaaf60d7280
caseone <421325991@qq.com>
2020-07-20 20:49:46 +0800

    1

--- java/java_test/test/src/JavaTest.java
+++ java/java_test/test/src/JavaTest.java
@@ -1,9 +1,19 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class JavaTest {
 
+    public static void f1(){
+        List<String> list= Arrays.asList("a", "b", "c", "d");
 
-
+        List<String> collect =list.stream().map(String::toUpperCase).collect(Collectors.toList());
+        System.out.println(collect); //[A, B, C, D]
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("main start...");
+
+        f1();
     }
 }
